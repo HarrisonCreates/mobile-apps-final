8f7961146da7e8439995ad3e41768c83538d7ada
Harrison Burr <harrison.burr@uconn.edu>
2020-04-13 10:57:04 -0400

    Uploading firebase functionality

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,5 @@
+console.log(localStorage.selected_categories);
+
 document.querySelector('.update_selection_btn').addEventListener('click', () => {
    window.location = "category_selection.html";
 });
---
+++ firebase_functions.js
@@ -0,0 +1,9 @@
+function getAllRelevantPosts(query) {
+    let relevant_returns = {};
+
+
+
+    //db.collection("posts").limit(200).get().then(() => {
+
+    });
+}
