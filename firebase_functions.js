2083290ccf3d50c9d90439041729515c383082ed
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-21 15:28:54 -0400

    Trying again

--- firebase_functions.js
+++ firebase_functions.js
@@ -1,6 +1,6 @@
 function getAllRelevantPosts(query) {
   let relevant_returns = {};
-  query.forEach((item) => {
+  query.forEach((item, i) => {
     console.log("Retrieving " + item);
     db.collection("posts").limit(200).get().then((item) => {
       returned_vals.forEach((result, i) => {
