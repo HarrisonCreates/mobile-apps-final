597815ed1dfeb4d3ba52f7b24d6a3f95867e836f
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-21 15:34:36 -0400

    Goofed up should be fixed now

--- firebase_functions.js
+++ firebase_functions.js
@@ -1,7 +1,13 @@
 function getAllRelevantPosts(query) {
   let relevant_returns = {};
-  console.log("Received: " + query);
-  query.forEach((item, i) => {
+  query_array = [];
+  // Converting query dictionary to array
+  Object.keys(query).forEach((item, i) => {
+    query_array.push(item);
+  });
+  console.log("Received:");
+  console.log(query_array);
+  query_array.forEach((item, i) => {
     console.log("Retrieving " + item);
     db.collection("posts").limit(200).get().then((item) => {
       returned_vals.forEach((result, i) => {
