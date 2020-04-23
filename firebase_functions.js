4236bb041580868372da08056f7ba07c41ebfe76
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-23 15:10:47 -0400

    new rabbit hole

--- firebase_functions.js
+++ firebase_functions.js
@@ -13,15 +13,19 @@ function getAllRelevantPosts(query) {
     var doc_ref = db.collection("posts").doc(item).collection("uploads").orderBy("likes", "desc");
     doc_ref.get().then((returned_vals) => {
       returned_vals.forEach((doc) => {
-        var this_doc = doc.data();
-        var new_key = "entry_" + num;
-        temp_array[new_key] = this_doc;
-        num += 1
+        var this_doc = doc.data().resolve().then((final_doc) => {
+          console.log("We're Working With:");
+          console.log(final_doc);
+          console.log(type(final_doc));
+          //var new_key = "entry_" + num;
+          //temp_array[new_key] = this_doc;
+          //num += 1
+        });
       });
     });
-    var new_item = item.toString().replace("-", "_");
-    new_item = new_item.replace(" ", "_");
-    relevant_returns[new_item] = temp_array;
+    //var new_item = item.toString().replace("-", "_");
+    //new_item = new_item.replace(" ", "_");
+    //relevant_returns[new_item] = temp_array;
   });
   try {
     return ["true", relevant_returns];
