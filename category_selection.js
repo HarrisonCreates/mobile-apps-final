05923fa2f149bcef1856fb9b24e5db24f6e09961
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-25 12:37:55 -0400

    Beautifying and debugging

--- category_selection.js
+++ category_selection.js
@@ -1,55 +1,52 @@
 var user_selections = {};
 var items;
 
-if(localStorage.selected_categories != undefined){
-   for(var i = 0; i < document.querySelector('.options').children.length; i++){
-     let this_object = document.querySelector('.options').children[i];
-     let this_value = this_object.innerHTML.toLowerCase();
-     if(localStorage.selected_categories.includes(this_value)){
-       this_object.className = 'enabled';
-       user_selections[this_value] = true;
-     }
-   }
-   document.querySelector('img').src = 'assets/arrow_svg.svg';
+if (localStorage.selected_categories != undefined) {
+  for (var i = 0; i < document.querySelector('.options').children.length; i++) {
+    let this_object = document.querySelector('.options').children[i];
+    let this_value = this_object.innerHTML.toLowerCase();
+    if (localStorage.selected_categories.includes(this_value)) {
+      this_object.className = 'enabled';
+      user_selections[this_value] = true;
+    }
+  }
+  document.querySelector('img').src = 'assets/arrow_svg.svg';
 } else {
   document.querySelector('img').src = 'assets/arrow_svg_gray.svg';
 }
 
-function check_me(label){
+function check_me(label) {
   let result = label.className;
   let result_item = label.innerHTML.toLowerCase();
-   if(result == 'disabled'){
-     label.className = 'enabled';
-     user_selections[result_item] = true;
-     document.querySelector('img').src = 'assets/arrow_svg.svg';
-   } else {
-     label.className = 'disabled';
-     delete user_selections[result_item];
-     if(Object.keys(user_selections).length == 0){
-       document.querySelector('img').src = 'assets/arrow_svg_gray.svg';
-     }
-   }
+  if (result == 'disabled') {
+    label.className = 'enabled';
+    user_selections[result_item] = true;
+    document.querySelector('img').src = 'assets/arrow_svg.svg';
+  } else {
+    label.className = 'disabled';
+    delete user_selections[result_item];
+    if (Object.keys(user_selections).length == 0) {
+      document.querySelector('img').src = 'assets/arrow_svg_gray.svg';
+    }
+  }
 }
 
 document.querySelector('img').addEventListener('click', async () => {
-   if(Object.keys(user_selections).length > 0){
-     update_localstorage_vars(Object.keys(user_selections));
-     getAllRelevantPosts(user_selections, function(res) {
-        if(res){
-          items = res;
-          console.log("Received:");
-          console.log(items);
-          console.log("Attempting to Stringify...");
-          console.log(JSON.stringify(items));
-        } else {
-          alert("Something went wrong. Check your internet connection.");
-        }
-     }
-    );
+  if (Object.keys(user_selections).length > 0) {
+    update_localstorage_vars(Object.keys(user_selections));
+    getAllRelevantPosts(user_selections, function(res) {
+      if (res) {
+        items = res;
+      } else {
+        alert("Something went wrong. Check your internet connection.");
+      }
+    });
+    console.log(items);
+    console.log(JSON.stringify(items));
   }
 });
 
-function remind_purpose(){
+function remind_purpose() {
   let message = 'Select one or multiple options to proceed';
   alert(message);
 }
