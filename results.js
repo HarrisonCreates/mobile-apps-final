c60939b4b38d7cd62ad7c4e972fc64f77d7607af
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-25 15:07:47 -0400

    progressing along

--- results.js
+++ results.js
@@ -5,6 +5,15 @@ var this_category_results = categories[category_name];
 var title = document.querySelector('.text_collection > h1');
 var subtext = document.querySelector('.text_collection > h3');
 
-console.log(this_category_results);
+Object.keys(this_category_results).forEach((item) => {
+   // Basically, create a new div per item
+});
 
 // Put a switch/case area here to handle setting the titles
+if(category_name == "breakup_divorce"){
+   title.innerHTML = "HEARTBROKEN?";
+   subtext.innerHTML = "BROKEN DOORS ARE EASIER TO OPEN";
+} else if(category_name == "loneliness"){
+  title.innerHTML = "LONELY?";
+  subtext.innerHTML = "LET'S FIX THAT";
+}
