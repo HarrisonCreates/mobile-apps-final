b46af7fa1ed104b060369f93d352dd795541116c
HarrisonCreates <harrison.burr@uconn.edu>
2020-04-25 13:19:49 -0400

    This may be gucchi who knows

--- dashboard.js
+++ dashboard.js
@@ -13,7 +13,7 @@ Object.keys(categories).forEach((item) => {
   var temp_div = document.createElement('div');
   var temp_div_title = document.createElement('p');
   var temp_div_arrow = document.createElement('img');
-  temp_div.setAttribute('class', item + 'posts');
+  temp_div.setAttribute('class', item + '_posts');
   temp_div.setAttribute('onclick', 'process(this)');
   temp_div_arrow.setAttribute('src', 'assets/arrow_svg.svg');
   temp_div_title.innerHTML = '"' + item.toString().toUpperCase().replace(/_/g, " ") + '"' + " POSTS";
