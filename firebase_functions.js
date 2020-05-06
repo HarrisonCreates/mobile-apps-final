910af329711577a5cf3f735fb9d837ed7aef5c32
HarrisonCreates <harrison.burr@uconn.edu>
2020-05-06 10:43:56 -0400

    this may break

--- firebase_functions.js
+++ firebase_functions.js
@@ -16,7 +16,9 @@ function getAllRelevantPosts(query, callback) {
       returned_vals.forEach((doc) => {
         var this_doc = doc.data();
         var new_key = "entry_" + num;
+        var id = doc.id();
         temp_array[new_key] = this_doc;
+        temp_array['id'] = id;
         var new_item = item.toString().replace(/-/g, "_");
         relevant_returns[new_item] = temp_array;
         num += 1
@@ -27,11 +29,8 @@ function getAllRelevantPosts(query, callback) {
 }
 
 function unlike_post(query, callback) {
-  console.log("Received:");
-  console.log(query);
   let category = query[0];
   let post = query[1];
-  console.log(category + ", " + post);
 
   let doc_to_change = db.collection("posts").doc(category).collection("uploads").doc(post);
 
@@ -39,31 +38,25 @@ function unlike_post(query, callback) {
     console.log(found_post);
     console.log(found_post.data());
   });
-
-  console.log(doc_to_change);
 /*
   var updating = doc_to_change.set({
-    likes: doc_to_change[] + 1
+    likes: 701
 }, { merge: true });
 */
   callback(true);
 }
 
 function like_post(query, callback) {
-  console.log("Received:");
-  console.log(query);
   let category = query[0];
   let post = query[1];
-  console.log(category + ", " + post);
 
   let doc_to_change = db.collection("posts").doc(category).collection("uploads").doc(post);
 
   doc_to_change.get().then((found_post) => {
+    console.log("Found Post:");
     console.log(found_post);
     console.log(found_post.data());
   });
-
-  console.log(doc_to_change);
 
   callback(true);
   /*
